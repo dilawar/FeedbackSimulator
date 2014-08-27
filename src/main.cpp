91ecccbaf370f773849697e1151ac928da126a76
Dilawar <dilawars@iitb.ac.in>
2014-08-27 14:06:01 +0530

    Adding CMakeLists.txt file. Not tested yet.

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * ==============================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  Entry point of this application.
+ *
+ *        Version:  1.0
+ *        Created:  Wednesday 27 August 2014 01:43:05  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dilawar Singh (), dilawars@ncbs.res.in
+ *   Organization:  
+ *
+ * ==============================================================================
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    
+    cerr << "I am here" << endl;
+    return 0;
+}
