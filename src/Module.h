d57144bec6ba034f2e8a05641190df2911e72a88
Dilawar <dilawars@iitb.ac.in>
2014-09-06 22:18:59 +0530

    Added Module class.

---
+++ src/Module.h
@@ -0,0 +1,32 @@
+/*
+ * ==============================================================================
+ *
+ *       Filename:  Module.h
+ *
+ *    Description:  Module is most primitive system in this application.
+ *
+ *        Version:  1.0
+ *        Created:  Saturday 06 September 2014 09:53:57  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dilawar Singh (), dilawars@ncbs.res.in
+ *   Organization:  
+ *
+ * ==============================================================================
+ */
+
+#ifndef  MODULE_INC
+#define  MODULE_INC
+
+class Module
+{
+    public:
+        Module();
+        ~Module();
+
+    private:
+        /* data */
+};
+
+#endif   /* ----- #ifndef MODULE_INC  ----- */
